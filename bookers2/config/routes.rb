aa021d4465e8d6f0853e329ad07e42f25c95f6fb
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-11 13:10:55 +0000

    [add]about

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'users/update'
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

33673cd7df8f27762b262fa1f0e0253c553366e4
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-12 07:38:38 +0000

    [add]index

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,15 +1,22 @@
 class BooksController < ApplicationController
-  
+
   def create
   end
-  
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
-  
+
   def show
   end
-  
+
   def destroy
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :destroy]
+  post 'books' => 'books#create'
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

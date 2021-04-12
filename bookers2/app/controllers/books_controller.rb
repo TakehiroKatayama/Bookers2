21acecfa05db7cf578e068fd6186a0bf3dd1b625
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-12 14:25:35 +0000

    [add]books_create

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -9,6 +13,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
@@ -16,7 +21,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body, :image, :user_id)
   end
-
+あ
 end

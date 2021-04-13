aba17d7eb15762bfc60296f4b63d6d7a1f3734d6
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-13 16:14:47 +0000

    [add]link

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def show
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def edit
